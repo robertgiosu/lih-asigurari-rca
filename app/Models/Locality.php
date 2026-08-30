b93e5c9e11e9a98eb1f28ecd4ab6539615e2c3b2
Robert Giosu <robertgiosu@gmail.com>
2026-08-30 14:11:57 +0300

    Add Locality model class

---
+++ app/Models/Locality.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+#[Fillable(['county_code', 'name', 'siruta'])]
+class Locality extends Model
+{
+    public function county(): BelongsTo
+    {
+        return $this->belongsTo(County::class, 'county_code', 'code');
+    }
+
+    /** Numele pentru interfata. 'name' ramane neatins - el pleaca spre API.
+     */
+    public function displayName(): string
+    {
+        return Str::title($this->name);
+    }
+}
