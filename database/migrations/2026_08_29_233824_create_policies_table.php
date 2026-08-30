e82170631da6d526750f23b1bd9a06110dde6964
Dusa Razvan <razvandusa0608@gmail.com>
2026-08-30 13:25:23 +0300

    Add migration explanation

--- database/migrations/2026_08_29_233824_create_policies_table.php
+++ database/migrations/2026_08_29_233824_create_policies_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Dupa ce un utilizatorul a ales o oferta a emis polita. Tabelul policies contine datele politei.
 return new class extends Migration
 {
     /**
